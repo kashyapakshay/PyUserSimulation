070f7401e768aa63c289e7f57b65d7e97ca257f0
Akshay Kashyap <kashyapa@union.edu>
2016-10-22 20:50:17 -0400

    Skeleton Code for n-gram based simulation

---
+++ usersimulation.py
@@ -0,0 +1,24 @@
+class UserSimulation:
+    def __init__(self):
+        pass
+
+    def recordAction(self, userAction, systemAction):
+        pass
+
+    def addNewUserAction(self, newUserAction):
+        pass
+
+    def addNewSystemAction(self, newSystemAction):
+        pass
+
+    def updateUserActionCount(self, userAction, systemAction):
+        pass
+
+    def getCount(self, userAction, systemAction):
+        pass
+
+    def getProbability(self, userAction, systemAction):
+        pass
+
+    def getPredictedUserAction(self, systemAction):
+        pass
